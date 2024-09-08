527e3d42741d6f555e48af1d38633409eb845ff9
refatahmed441 <refatahmed441@gmail.com>
2024-09-08 13:48:19 +0600

    Package Page:
    This is Our list package sesction in package page

--- src/components/Package/Card/OurListPackage.js
+++ src/components/Package/Card/OurListPackage.js
@@ -13,26 +13,109 @@ const OurListPackage = () => {
             </div>
 
             <div className='mt-12'>
-                <div className="grid grid-cols-3 gap-8">
-                    <div className='h-[426px]'>
-                        <div className='h-[182px] text-left text-white' style={{ background: 'linear-gradient(to top,  #111111, #A9A9A9'}}>
+                <div className="grid grid-cols-3 gap-8 ">
+                    <div className='h-[426px] bg-[#F5F5F5] rounded-lg' style={{ border: '1px solid #F5F5F5' }}>
+                        <div className='h-[182px] text-left text-white rounded-t-lg' style={{ background: 'linear-gradient(to top, #111111, #A9A9A9)' }}>
                             <p className='Gothic-A1-400 pt-[142px] pl-4'>Start From: <span className='goldenText'>$2,200</span> (9 days)</p>
                         </div>
-                        <div className='h-[244px] mx-3 my-4'>
-                            <h2 className='scheherazade-thin-normal text-[32px] leading-8'>Unforgettable Premium Hajj Experience: Journey to Spiritual Enlightenment</h2>
+                        <div className='mx-3 my-4'>
+                            <h2 className='scheherazade-thin-normal text-[32px] leading-8'>
+                                Unforgettable Premium Hajj Experience: Journey to Spiritual Enlightenment
+                            </h2>
                             <p className='Gothic-A1-thin mt-2'>Indulge in luxurious accommodations, exclusive access to sacred sites, personalized guidance..</p>
                             <div>
-                                <button className="w-full  mt-3 h-[48px] rounded bg-[#D19B56] text-white hover:bg-black Gothic-A1-700 font-bold">
+                                <button className="w-full mt-3 h-[48px] rounded bg-[#D19B56] text-white hover:bg-black Gothic-A1-700 font-bold">
                                     More Details &nbsp; {'->'}
                                 </button>
                             </div>
                         </div>
                     </div>
-                    <div>02</div>
-                    <div>03</div>
-                    <div>04</div>
-                    <div>05</div>
-                    <div>06</div>
+
+                    <div className='h-[426px] bg-[#F5F5F5] rounded-lg' style={{ border: '1px solid #F5F5F5' }}>
+                        <div className='h-[182px] text-left text-white rounded-t-lg' style={{ background: 'linear-gradient(to top, #111111, #A9A9A9)' }}>
+                            <p className='Gothic-A1-400 pt-[142px] pl-4'>Start From: <span className='goldenText'>$2,200</span> (9 days)</p>
+                        </div>
+                        <div className='mx-3 my-4'>
+                            <h2 className='scheherazade-thin-normal text-[32px] leading-8'>
+                                Unforgettable Premium Hajj Experience: Journey to Spiritual Enlightenment
+                            </h2>
+                            <p className='Gothic-A1-thin mt-2'>Indulge in luxurious accommodations, exclusive access to sacred sites, personalized guidance..</p>
+                            <div>
+                                <button className="w-full mt-3 h-[48px] rounded bg-[#D19B56] text-white hover:bg-black Gothic-A1-700 font-bold">
+                                    More Details &nbsp; {'->'}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='h-[426px] bg-[#F5F5F5] rounded-lg' style={{ border: '1px solid #F5F5F5' }}>
+                        <div className='h-[182px] text-left text-white rounded-t-lg' style={{ background: 'linear-gradient(to top, #111111, #A9A9A9)' }}>
+                            <p className='Gothic-A1-400 pt-[142px] pl-4'>Start From: <span className='goldenText'>$2,200</span> (9 days)</p>
+                        </div>
+                        <div className='mx-3 my-4'>
+                            <h2 className='scheherazade-thin-normal text-[32px] leading-8'>
+                                Unforgettable Premium Hajj Experience: Journey to Spiritual Enlightenment
+                            </h2>
+                            <p className='Gothic-A1-thin mt-2'>Indulge in luxurious accommodations, exclusive access to sacred sites, personalized guidance..</p>
+                            <div>
+                                <button className="w-full mt-3 h-[48px] rounded bg-[#D19B56] text-white hover:bg-black Gothic-A1-700 font-bold">
+                                    More Details &nbsp; {'->'}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className='h-[426px] bg-[#F5F5F5] rounded-lg' style={{ border: '1px solid #F5F5F5' }}>
+                        <div className='h-[182px] text-left text-white rounded-t-lg' style={{ background: 'linear-gradient(to top, #111111, #A9A9A9)' }}>
+                            <p className='Gothic-A1-400 pt-[142px] pl-4'>Start From: <span className='goldenText'>$2,200</span> (9 days)</p>
+                        </div>
+                        <div className='mx-3 my-4'>
+                            <h2 className='scheherazade-thin-normal text-[32px] leading-8'>
+                                Unforgettable Premium Hajj Experience: Journey to Spiritual Enlightenment
+                            </h2>
+                            <p className='Gothic-A1-thin mt-2'>Indulge in luxurious accommodations, exclusive access to sacred sites, personalized guidance..</p>
+                            <div>
+                                <button className="w-full mt-3 h-[48px] rounded bg-[#D19B56] text-white hover:bg-black Gothic-A1-700 font-bold">
+                                    More Details &nbsp; {'->'}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='h-[426px] bg-[#F5F5F5] rounded-lg' style={{ border: '1px solid #F5F5F5' }}>
+                        <div className='h-[182px] text-left text-white rounded-t-lg' style={{ background: 'linear-gradient(to top, #111111, #A9A9A9)' }}>
+                            <p className='Gothic-A1-400 pt-[142px] pl-4'>Start From: <span className='goldenText'>$2,200</span> (9 days)</p>
+                        </div>
+                        <div className='mx-3 my-4'>
+                            <h2 className='scheherazade-thin-normal text-[32px] leading-8'>
+                                Unforgettable Premium Hajj Experience: Journey to Spiritual Enlightenment
+                            </h2>
+                            <p className='Gothic-A1-thin mt-2'>Indulge in luxurious accommodations, exclusive access to sacred sites, personalized guidance..</p>
+                            <div>
+                                <button className="w-full mt-3 h-[48px] rounded bg-[#D19B56] text-white hover:bg-black Gothic-A1-700 font-bold">
+                                    More Details &nbsp; {'->'}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='h-[426px] bg-[#F5F5F5] rounded-lg' style={{ border: '1px solid #F5F5F5' }}>
+                        <div className='h-[182px] text-left text-white rounded-t-lg' style={{ background: 'linear-gradient(to top, #111111, #A9A9A9)' }}>
+                            <p className='Gothic-A1-400 pt-[142px] pl-4'>Start From: <span className='goldenText'>$2,200</span> (9 days)</p>
+                        </div>
+                        <div className='mx-3 my-4'>
+                            <h2 className='scheherazade-thin-normal text-[32px] leading-8'>
+                                Unforgettable Premium Hajj Experience: Journey to Spiritual Enlightenment
+                            </h2>
+                            <p className='Gothic-A1-thin mt-2'>Indulge in luxurious accommodations, exclusive access to sacred sites, personalized guidance..</p>
+                            <div>
+                                <button className="w-full mt-3 h-[48px] rounded bg-[#D19B56] text-white hover:bg-black Gothic-A1-700 font-bold">
+                                    More Details &nbsp; {'->'}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
