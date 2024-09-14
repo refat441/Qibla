410dcf8e403daee2afa3dd0fad8e452453508714
refatahmed441 <refatahmed441@gmail.com>
2024-09-14 17:09:10 +0600

    blog page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import BlogHeader from './Card/BlogHeader';
+import OurNews from './Card/OurNews';
 
 const Blog = () => {
     return (
         <div>
             {/* header */}
             <BlogHeader></BlogHeader>
+
+            {/* OUR News */}
+            <OurNews></OurNews>
         </div>
     );
 };
---
+++ src/components/Blog/Card/OurNews.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const OurNews = () => {
+    return (
+        <div className='mx-[100px]'>
+            <div className='overflow-hidden flex items-center justify-center mt-6'>
+                <img className='w-5 h-5 object-cover' src="/img/main-icon.svg" alt="Centered icon" />
+                <p className='Gothic-A1-900 text-[14px] goldenText pl-4'>OUR NEWS</p>
+            </div>
+            <div>
+                <h1 className='scheherazade-thin text-[64px] mt-4'>Latest News and Updates</h1>
+                <p className='Gothic-A1-thin mt-4'> Stay informed with of articles, announcements, and insights on all things related to travel, Umrah, and Hajj.</p>
+            </div>
+
+            <div className='mt-10' >
+                <div >
+
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default OurNews;
