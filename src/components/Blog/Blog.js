59a4139b98318ada4dcf4e21d6b5cb3135b0ed93
refatahmed441 <refatahmed441@gmail.com>
2024-09-15 13:49:40 +0600

    all updated in blog page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,6 +2,8 @@ import React from 'react';
 import BlogHeader from './Card/BlogHeader';
 import OurNews from './Card/OurNews';
 import PaginatinSection from './Card/PaginatinSection';
+import UmrahPackage from '../PlaceholderComponents/Cards/UmrahPackage';
+import Footer from '../PlaceholderComponents/Cards/Footer';
 
 const Blog = () => {
     return (
@@ -14,6 +16,12 @@ const Blog = () => {
 
             {/* PaginatinSection */}
             <PaginatinSection></PaginatinSection>
+
+            {/* PaginatinSection */}
+            <UmrahPackage></UmrahPackage>
+
+            {/* Footer */}
+            <Footer></Footer>
         </div>
     );
 };
